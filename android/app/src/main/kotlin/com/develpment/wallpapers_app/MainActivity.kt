893f03f5ca21a99121b78a7b568ce62f687670f0
Kokodavid <kingkgdave@gmail.com>
2021-02-10 01:37:49 +0300

    main branch initial commit

---
+++ android/app/src/main/kotlin/com/develpment/wallpapers_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.develpment.wallpapers_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/const.dart
@@ -0,0 +1,7 @@
+final String apiKey = "563492ad6f91700001000001edb6da4cbcbd4c37bc7fe6096c0f48f5";
+final String editorChoiceEndPoint =
+    "https://api.pexels.com/v1/search?query=wallpaper";
+final String perPageLimit = "&per_page=30";
+final String searchEndPoint = "https://api.pexels.com/v1/search?query=";
+final String message = "Check out this awesome wallpaper app:";
+final String url = "https://play.google.com/store/apps/details?id=";
