2e2d0eb05f13f69bfd1486154ee509ee93205a70
Kokodavid <kingkgdave@gmail.com>
2021-02-10 02:32:57 +0300

    Blocs files SetUp

---
+++ lib/Bloc/wallpaperBloc.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wallpapers_app/Bloc/WallpaperState.dart';
+import 'package:wallpapers_app/Bloc/wallpaperEvent.dart';
+import 'package:wallpapers_app/Model/wallpaper.dart';
+import 'package:http/http.dart' as http;
+import 'package:wallpapers_app/const.dart';
+
+
+class WallpaperBloc extends Bloc<WallpaperEvent, WallpaperState> {
+  @override
+  get initialState => WallpaperIsLoading();
+  List<Wallpaper> _wallpaper = List<Wallpaper>();
+
+  @override
+  Stream<WallpaperState> mapEventToState(WallpaperEvent event) async* {
+      if(event is GetAllWallpaper) {
+        yield WallpaperIsLoading();
+        try {
+          var response = await http
+              .get(Uri.encodeFull(editorChoiceEndPoint + perPageLimit), headers: {
+            "Accept": "application/json",
+            "Authorization": "$apiKey"
+          });
+          var data = jsonDecode(response.body)["photos"];
+          _wallpaper = List<Wallpaper>();
+          for (var i = 0; i < data.length;i++){
+            _wallpaper.add(Wallpaper.fromMap(data[i]));
+          }
+          yield WallpaperIsLoaded(_wallpaper);
+        } catch(_) {
+          yield WallpaperIsNotLoaded();
+        }
+      }
+  }
+
+}
